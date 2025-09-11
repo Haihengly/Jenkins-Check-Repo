c05e66c069a632d83c521f9193fb49f837da892d
Haihengly <your-email@example.com>
2025-09-11 15:24:57 +0700

    update

--- vars/checkoutrepo.groovy
+++ vars/checkoutrepo.groovy
@@ -1,9 +1,5 @@
 def call(String branch, String repoUrl) {
-    stage('Checkout') {
-        steps {
             script {
                 git branch: branch, url: repoUrl
             }
-        }
-    }
 }

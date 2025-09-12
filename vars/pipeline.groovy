119bd7eff71b3a2f459bb29292356c62cad94a26
Haihengly <your-email@example.com>
2025-09-12 10:58:58 +0700

    update

--- vars/pipeline.groovy
+++ vars/pipeline.groovy
@@ -4,39 +4,30 @@ def call() {
         environment {
             BOT_TOKEN = credentials('TELEGRAM_BOT_TOKEN')
             CHAT_ID   = credentials('TELEGRAM_CHAT_ID')
-            // BRANCH_NAME = 'main'
-            // REPO_URL = 'https://github.com/Haihengly/Products-Jenkins'
+            BRANCH_NAME = 'main'
+            REPO_URL = 'https://github.com/Haihengly/Products-Jenkins'
         }
         stages {
-            stage('Run All Stages') {
+            stage('Checkout') {
+                steps {
+                    checkoutrepo(BRANCH_NAME, REPO_URL)
+                }
+            }
+            stage('Build') {
                 steps {
                     script {
-                        myStages()
+                        build()
+                    }
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    script {
+                        deploy()
                     }
                 }
             }
         }
-        // stages {
-        //     stage('Checkout') {
-        //         steps {
-        //             checkoutrepo(BRANCH_NAME, REPO_URL)
-        //         }
-        //     }
-        //     stage('Build') {
-        //         steps {
-        //             script {
-        //                 build()
-        //             }
-        //         }
-        //     }
-        //     stage('Deploy') {
-        //         steps {
-        //             script {
-        //                 deploy()
-        //             }
-        //         }
-        //     }
-        // }
         post {
             always{
                 success { script { telegramNotify.notify("SUCCESS") } }

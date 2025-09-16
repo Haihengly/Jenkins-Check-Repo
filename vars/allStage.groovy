7d79bb8fc8d7392cd2c16f2703dd8759539a6128
Haihengly <your-email@example.com>
2025-09-16 17:18:55 +0700

    update

--- vars/allStage.groovy
+++ vars/allStage.groovy
@@ -1,37 +1,3 @@
-// def call(Map config) {
-//     return [
-//         [
-//             name: 'Checkout Repo Stage',
-//             action: { -> checkoutrepo(config)
-//                 // checkoutrepo([
-//                 //     branch: config.branch,
-//                 //     repoUrl: config.repoUrl
-//                 // ])
-//             }
-//         ],
-//         [
-//             name: 'Build Stage',
-//             action: { -> build(config)
-//                 // if (config.build) { 
-//                 //     build(config)
-//                 // } else {
-//                 //     echo "Skipping build"
-//                 // }
-//             }
-//         ],
-//         [
-//             name: 'Deploy Stage',
-//             action: { -> deploy(config)
-//                 // if (config.deploy) {
-//                 //     deploy(config)
-//                 // } else {
-//                 //     echo "Skipping deploy"
-//                 // }
-//             }
-//         ]
-//     ]
-// }
-
 def call(Map config) {
     def buildFlag  = config.get('build', true)
     def deployFlag = config.get('deploy', true)

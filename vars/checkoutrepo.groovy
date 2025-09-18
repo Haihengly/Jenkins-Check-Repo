883355a4fe6da5dca7c4cfea4b0670a82164c372
Haihengly <your-email@example.com>
2025-09-18 17:48:46 +0700

    update

--- vars/checkoutrepo.groovy
+++ vars/checkoutrepo.groovy
@@ -1,20 +1,31 @@
-def call(Map config) {
-    def CLONE_DIR = "${env.HOME}/code-clone/"
+// def call(Map config) {
+//     def CLONE_DIR = "${env.HOME}/code-clone/"
 
 
+
+//     sh """
+//         rm -rf ${CLONE_DIR}
+//         cd ${env.HOME}
+//         git clone ${config.repoUrl}
+//         ls -l ${CLONE_DIR}
+//     """
+
+
+//     // git (
+//     //     branch: config.branch, 
+//     //     url: config.repoUrl, 
+//     //     credentialsId: 'git_token'
+//     // )
+// }
+ 
+
+ def call(Map config) {
+    def CLONE_DIR = "${env.HOME}/code-clone"
 
     sh """
         rm -rf ${CLONE_DIR}
         cd ${env.HOME}
-        git clone ${config.repoUrl}
+        git clone ${config.repoUrl} code-clone
         ls -l ${CLONE_DIR}
     """
-
-
-    // git (
-    //     branch: config.branch, 
-    //     url: config.repoUrl, 
-    //     credentialsId: 'git_token'
-    // )
 }
- 

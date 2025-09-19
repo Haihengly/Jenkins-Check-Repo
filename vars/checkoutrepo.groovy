ca14f5c3f798c2948d76fca27ad5eb915e22f63a
Haihengly <your-email@example.com>
2025-09-19 14:41:03 +0700

    updage

--- vars/checkoutrepo.groovy
+++ vars/checkoutrepo.groovy
@@ -8,7 +8,7 @@ def call(Map config) {
             git clone -b ${config.branch} ${config.repoUrl} ${CLONE_DIR}/${config.BUILD_DIR}/App-clone
         fi
 
-        sh 'ls -l /var/jenkins_home/My-Docker/Dev-Service/docker-compose.yml'
+        ls -l /var/jenkins_home/My-Docker/Dev-Service/docker-compose.yml
 
         # Ensure only app code is refreshed, Dockerfile/compose stay safe
         ls -l ${CLONE_DIR}/${config.BUILD_DIR}/App-clone

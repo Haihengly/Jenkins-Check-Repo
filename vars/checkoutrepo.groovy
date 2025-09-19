05a7c95c9a7935b9ed2cfcdbd27a85357bfcfed9
Haihengly <your-email@example.com>
2025-09-19 15:22:56 +0700

    -

--- vars/checkoutrepo.groovy
+++ vars/checkoutrepo.groovy
@@ -3,7 +3,7 @@ def call(Map config) {
 
     sh """
         rm -rf ${CLONE_DIR}/${config.BUILD_DIR} 
-        ls -la ${CLONE_DIR}
+        ls -la ${CLONE_DIR}/
         git clone -b ${config.branch} ${config.REPO_URL} ${CLONE_DIR}/${config.BUILD_DIR}
         ls -la ${CLONE_DIR}/${config.BUILD_DIR}
     """

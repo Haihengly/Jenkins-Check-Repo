c5cda4c867dbb414e6b631a3c231c4b8d31212ce
Haihengly <your-email@example.com>
2025-09-25 14:35:21 +0700

    update

--- vars/checkoutrepo.groovy
+++ vars/checkoutrepo.groovy
@@ -1,11 +1,32 @@
+// def call(Map config) {
+//     def CLONE_DIR = "/My-Docker/Dev-Service"
+
+//     sh """
+//         rm -rf ${CLONE_DIR}/${config.BUILD_DIR} 
+//         pwd
+//         ls -la ${CLONE_DIR}/
+//         git clone -b ${config.branch} ${config.REPO_URL} ${CLONE_DIR}/${config.BUILD_DIR}
+//         ls -la ${CLONE_DIR}/${config.BUILD_DIR}
+//     """
+// }
+
+
 def call(Map config) {
     def CLONE_DIR = "/My-Docker/Dev-Service"
 
+    // Clone the repo
     sh """
-        rm -rf ${CLONE_DIR}/${config.BUILD_DIR} 
-        pwd
-        ls -la ${CLONE_DIR}/
+        rm -rf ${CLONE_DIR}/${config.BUILD_DIR}
+        mkdir -p ${CLONE_DIR}
         git clone -b ${config.branch} ${config.REPO_URL} ${CLONE_DIR}/${config.BUILD_DIR}
-        ls -la ${CLONE_DIR}/${config.BUILD_DIR}
     """
+
+    // Capture the latest commit message
+    def lastCommitMsg = sh(
+        script: "cd ${CLONE_DIR}/${config.BUILD_DIR} && git log -1 --pretty=format:'%s'",
+        returnStdout: true
+    ).trim()
+
+    echo "Latest commit message: ${lastCommitMsg}"
+    return lastCommitMsg
 }

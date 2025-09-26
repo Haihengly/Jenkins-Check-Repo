7d30c03495d1a92cf31d8c77119f3023d531b1d6
Haihengly <your-email@example.com>
2025-09-26 14:18:14 +0700

    update

--- vars/allStage.groovy
+++ vars/allStage.groovy
@@ -1,3 +1,40 @@
+// def call(Map config) {
+//     def buildFlag  = config.get('build', true)
+//     def deployFlag = config.get('deploy', true)
+
+//     return [
+//         [
+//             name: 'Checkout',
+//             action: { ->
+//                 echo "Checking out branch ${config.branch}"
+//                 checkoutrepo(config)
+//             }
+//         ],
+//         [
+//             name: 'Build',
+//             action: { ->
+//                 if (buildFlag) {
+//                     echo "Building version ${config.version}"
+//                     build(config)
+//                 } else {
+//                     echo "Skipping build stage"
+//                 }
+//             }
+//         ],
+//         [
+//             name: 'Deploy',
+//             action: { ->
+//                 if (deployFlag) {
+//                     echo "Deploying version ${config.version} to ${config.envName}"
+//                     deploy(config)
+//                 } else {
+//                     echo "Skipping deploy stage"
+//                 }
+//             }
+//         ]
+//     ]
+// }
+
 def call(Map config) {
     def buildFlag  = config.get('build', true)
     def deployFlag = config.get('deploy', true)
@@ -7,6 +44,16 @@ def call(Map config) {
             name: 'Checkout',
             action: { ->
                 echo "Checking out branch ${config.branch}"
+                
+                // SCM detection in Jenkins workspace
+                node {
+                    dir("${env.WORKSPACE}/.scm-detect") {
+                        deleteDir() // optional: clean old checkout
+                        git branch: config.branch, url: config.REPO_URL
+                    }
+                }
+
+                // Your separate Docker clone
                 checkoutrepo(config)
             }
         ],

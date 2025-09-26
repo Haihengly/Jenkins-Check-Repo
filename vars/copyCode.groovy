e4d4ffb1d503ea832515283f8b7b96218a8825f1
Haihengly <your-email@example.com>
2025-09-26 15:08:57 +0700

    delete file copy

--- vars/copyCode.groovy
+++
@@ -1,7 +0,0 @@
-def call(Map config) {
-    def CLONE_DIR = "${env.HOME}/code-clone"
-    def STORE_DIR = "${env.HOME}/My-Docker"
-
-    // Copy code into Docker context
-    sh "cp -r ${CLONE_DIR}/${config.REPOSITORY_NAME}/. ${STORE_DIR}/Dev-Service/"
-}

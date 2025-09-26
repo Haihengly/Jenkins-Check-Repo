f47df2f7f052d6fef60a2d5e976ed83d27e9e798
Haihengly <your-email@example.com>
2025-09-26 14:43:11 +0700

    update

--- vars/checkoutrepo.groovy
+++ vars/checkoutrepo.groovy
@@ -1,20 +1,24 @@
 def call(Map config) {
-
-    // SCM check in Jenkins workspace
-    dir("${env.WORKSPACE}/.scm-detect") {
-        checkout([$class: 'GitSCM',
-                  branches: [[name: "*/${config.branch}"]],
-                  userRemoteConfigs: [[url: config.REPO_URL]]])
-    }
-
     def CLONE_DIR = "/My-Docker/Dev-Service"
 
+
+    dir("${env.WORKSPACE}/.scm-detect") {
+      checkout([$class: 'GitSCM',
+        branches: [[name: config.branch]],
+        userRemoteConfigs: [[url: config.REPO_URL]]
+      ])
+    }
     sh """
-        rm -rf ${CLONE_DIR}/${config.BUILD_DIR} 
-        pwd
-        ls -la ${CLONE_DIR}/
-        git clone -b ${config.branch} ${config.REPO_URL} ${CLONE_DIR}/${config.BUILD_DIR}
-        ls -la ${CLONE_DIR}/${config.BUILD_DIR}
+      rm -rf ${CLONE_DIR}/${config.BUILD_DIR}
+      cp -r ${env.WORKSPACE}/.scm-detect ${CLONE_DIR}/${config.BUILD_DIR}
     """
+
+    // sh """
+    //     rm -rf ${CLONE_DIR}/${config.BUILD_DIR} 
+    //     pwd
+    //     ls -la ${CLONE_DIR}/
+    //     git clone -b ${config.branch} ${config.REPO_URL} ${CLONE_DIR}/${config.BUILD_DIR}
+    //     ls -la ${CLONE_DIR}/${config.BUILD_DIR}
+    // """
 
 }

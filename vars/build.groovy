82600267feff39df542861b905fab21b04932b8a
Haihengly <your-email@example.com>
2025-09-26 15:55:33 +0700

    update

--- vars/build.groovy
+++ vars/build.groovy
@@ -1,10 +1,12 @@
 def call(Map config) { 
     def STORE_DIR = "/My-Docker/Dev-Service"
+    def envFile = config.envName == "prod" ? ".env.prod" : ".env.dev"
+    def composeFiles = "-f docker-compose.base.yml -f docker-compose.${config.envName}.yml"
 
     echo "Building Docker images with docker-compose..."
     sh """
         cd ${STORE_DIR}
-        docker-compose version
-        docker-compose build
+        docker compose ${composeFiles} --env-file ${envFile} version
+        docker compose ${composeFiles} --env-file ${envFile} build
     """
 }
--- vars/deploy.groovy
+++ vars/deploy.groovy
@@ -1,11 +1,14 @@
 def call(Map config) {
     def STORE_DIR = "/My-Docker/Dev-Service"
+    def envFile = config.envName == "prod" ? ".env.prod" : ".env.dev"
+    def composeFiles = "-f docker-compose.base.yml -f docker-compose.${config.envName}.yml"
 
     echo 'Starting containers...'
     sh """
         cd ${STORE_DIR}
-        docker-compose down
-        docker-compose up -d
+        docker compose ${composeFiles} --env-file ${envFile} down
+        docker compose ${composeFiles} --env-file ${envFile} up -d
+        
     """
 
 }

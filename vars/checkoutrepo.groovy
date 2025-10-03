315ccab99ef55cca36c8baae5cda79ac8c5c2d2f
Haihengly <your-email@example.com>
2025-10-03 17:22:24 +0700

    revert

--- vars/checkoutrepo.groovy
+++ vars/checkoutrepo.groovy
@@ -1,8 +1,6 @@
 def call(Map config) {
     def STORE_DIR = "/My-Docker/Dev-Service"
-    def TMP_DIR = "/tmp/${config.FOLDER}-${config.envName}"
 
-    // Checkout repo in Jenkins container
     dir("${env.WORKSPACE}/.scm-detect") {
       checkout([$class: 'GitSCM',
         branches: [[name: config.branch]],
@@ -10,23 +8,47 @@ def call(Map config) {
       ])
     }
 
-    // Copy files to a temp folder in Jenkins container
     sh """
-      rm -rf ${TMP_DIR}
-      mkdir -p ${TMP_DIR}
-      cp -r ${env.WORKSPACE}/.scm-detect/* ${TMP_DIR}
+      ssh jenkins@34.87.120.95 
+      rm -rf ${STORE_DIR}/${config.FOLDER}-${config.envName}
+      mkdir -p ${STORE_DIR}/${config.FOLDER}-${config.envName}
+      cp -r ${env.WORKSPACE}/.scm-detect/* ${STORE_DIR}/${config.FOLDER}-${config.envName}
+      ls ${env.WORKSPACE}/.scm-detect 
     """
 
-    // Transfer files to remote server and build/deploy
-    sh """
-      scp -r ${TMP_DIR} jenkins@34.87.120.95:${STORE_DIR}/
-      ssh jenkins@34.87.120.95 '
-        cd ${STORE_DIR}/${config.FOLDER}-${config.envName}
-        docker compose -f docker-compose.base.yml -f docker-compose.${config.envName}.yml --env-file .env.dev build
-        docker compose -f docker-compose.base.yml -f docker-compose.${config.envName}.yml --env-file .env.dev up -d
-      '
-    """
 }
+
+
+// def call(Map config) {
+//     def STORE_DIR = "/My-Docker/Dev-Service"
+//     def TMP_DIR = "/tmp/${config.FOLDER}-${config.envName}"
+
+//     // Checkout repo in Jenkins container
+//     dir("${env.WORKSPACE}/.scm-detect") {
+//       checkout([$class: 'GitSCM',
+//         branches: [[name: config.branch]],
+//         userRemoteConfigs: [[url: config.REPO_URL]]
+//       ])
+//     }
+
+//     // Copy files to a temp folder in Jenkins container
+//     sh """
+//       rm -rf ${TMP_DIR}
+//       mkdir -p ${TMP_DIR}
+//       cp -r ${env.WORKSPACE}/.scm-detect/* ${TMP_DIR}
+//     """
+
+//     // Transfer files to remote server and build/deploy
+//     sh """
+//       scp -r ${TMP_DIR} jenkins@34.87.120.95:${STORE_DIR}/
+//       ssh jenkins@34.87.120.95 '
+//         cd ${STORE_DIR}/${config.FOLDER}-${config.envName}
+//         docker compose -f docker-compose.base.yml -f docker-compose.${config.envName}.yml --env-file .env.dev build
+//         docker compose -f docker-compose.base.yml -f docker-compose.${config.envName}.yml --env-file .env.dev up -d
+//       '
+//     """
+// }
+
 
 
     // sh """

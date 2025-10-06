339ce316d7e07441f4d3d3664640e3832a4f9421
Haihengly <your-email@example.com>
2025-10-06 16:51:33 +0700

    ssh to vm6

--- vars/checkoutrepo.groovy
+++ vars/checkoutrepo.groovy
@@ -1,24 +1,3 @@
-// def call(Map config) {
-//     def STORE_DIR = "/My-Docker/Dev-Service"
-
-//     dir("${env.WORKSPACE}/.scm-detect") {
-//       checkout([$class: 'GitSCM',
-//         branches: [[name: config.branch]],
-//         userRemoteConfigs: [[url: config.REPO_URL]]
-//       ])
-//     }
-
-//     sh """
-//       ssh jenkins@34.87.120.95 
-//       rm -rf ${STORE_DIR}/${config.FOLDER}-${config.envName}
-//       mkdir -p ${STORE_DIR}/${config.FOLDER}-${config.envName}
-//       cp -r ${env.WORKSPACE}/.scm-detect/* ${STORE_DIR}/${config.FOLDER}-${config.envName}
-//       ls ${env.WORKSPACE}/.scm-detect 
-//     """
-
-// }
-
-
 def call(Map config) {
     def STORE_DIR = "/My-Docker/Dev-Service"
     def TMP_DIR = "/tmp/${config.FOLDER}-${config.envName}"
@@ -43,13 +22,3 @@ def call(Map config) {
       scp -r ${TMP_DIR} jenkins@vm6:${STORE_DIR}/
     """
 }
-
-
-
-    // sh """
-    //     rm -rf ${CLONE_DIR}/${config.BUILD_DIR} 
-    //     pwd
-    //     ls -la ${CLONE_DIR}/
-    //     git clone -b ${config.branch} ${config.REPO_URL} ${CLONE_DIR}/${config.BUILD_DIR}
-    //     ls -la ${CLONE_DIR}/${config.BUILD_DIR}
-    // """
--- vars/sshRemote.groovy
+++
@@ -1,8 +0,0 @@
-def call() {
-    sh """
-      ssh jenkins@34.87.120.95
-      whoami 
-      cd /
-      ls -l
-    """
-}
